530c3b7070bf7d60788c628fa19070cef1d8f959
mlsandoval <michaelsandoval8@gmail.com>
2020-08-04 23:38:11 +0000

    menu close on icon click added, story edited

--- js/main.js
+++ js/main.js
@@ -22,39 +22,30 @@ Menu
 (function() {
 
 	var bodyEl = document.body,
-		//content = document.querySelector( '.content-wrap' ),
+		content = document.querySelector( '.content-wrap' ),
 		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
-		isOpen = false;
+		closebtn = document.getElementById( 'close-button' );
+
+		var iconList = document.getElementsByClassName('icon-list')
 
 	function init() {
 		initEvents();
 	}
 
 	function initEvents() {
+		console.log('navicons: ', iconList)
 		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}
-
-		/* close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
-			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
-				toggleMenu();
-			}
-		} );
-		*/
+		$(iconList).on('click', '#nav-icon', toggleMenu)
+		
 	}
 
 	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
+		if($(bodyEl).hasClass('show-menu')){
+			classie.remove( bodyEl, 'show-menu' )
+		}else{
+			classie.add( bodyEl, 'show-menu' )
 		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
-		isOpen = !isOpen;
+
 	}
 
 	init();
